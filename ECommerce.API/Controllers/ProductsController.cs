4bfa8ae7e7cdb0fcc1d37cbf9fd546bade356a31
amilaDulanjana <amiladulanjana@hotmail.com>
2019-06-16 23:54:09 +0530

    integerate api and productCatalog service via serive remote

---
+++ ECommerce.API/Controllers/ProductsController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ECommerce.API.Model;
+using ECommerce.ProductCatalog.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ServiceFabric.Services.Client;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ECommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly IProductCatalogService _catalogService;
+
+        public ProductsController()
+        {
+            _catalogService = ServiceProxy.Create<IProductCatalogService>(
+                new Uri("fabric:/ECommerce/ECommerce.ProductCatalog"),
+                new ServicePartitionKey(0));
+        }
+
+        // GET: api/<controller>
+        [HttpGet]
+        public async Task<IEnumerable<Product>> Get()
+        {
+            IEnumerable<Product> products;
+            try
+            {
+                products = await _catalogService.GetAllProducts();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            return products;
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public async Task Post([FromBody]Product product)
+        {
+            await _catalogService.AddProduct(product);
+        }
+
+    }
+}
---
+++ ECommerce.API/Model/ApiProduct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.API.Model
+{
+    public class ApiProduct
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public bool Availability { get; set; }
+    }
+}
